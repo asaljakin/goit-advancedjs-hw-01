d47aff0f79f891bb6fa95cf15fee2238a88201d8
Andrew <asaljakin@gmail.com>
2023-11-05 20:02:24 +0200

    create hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
+// Описаний в документації* 
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів*
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Adds imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li>
+            <a class="gallery__item gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+            </li>`
+    ),
+    ''
+);
+
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash.throttle";
 
+const localStorageKey = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+let dataForm = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+const { email, message } = form.elements;
+refreshPage();
+
+function refreshPage() {
+    if (dataForm) {
+        email.value = dataForm.email || '';
+        message.value = dataForm.message || '';
+    }
+}
+
+function onFormData(evt) {
+    dataForm[evt.target.name] = evt.target.value;
+    localStorage.setItem(localStorageKey, JSON.stringify(dataForm));
+}
+
+function onSubmitForm(evt) {
+    console.log(JSON.parse(localStorage.getItem(localStorageKey)));
+    
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(localStorageKey);
+    dataForm = {};
+}
